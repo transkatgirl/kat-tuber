749dd5e9fd55cd75757f754ba823d830b88392c8
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-03-10 13:22:21 -0700

    added conversion script

---
+++ convert.sh
@@ -0,0 +1,19 @@
+for outfit in generated/*; do
+	outfit_name=$(basename ${outfit})
+
+	for body in generated/${outfit_name}/*; do
+		body_name=$(basename ${body})
+
+		for eyes in generated/${outfit_name}/${body_name}/*; do
+			eyes_name=$(basename ${eyes})
+
+			for faces in generated/${outfit_name}/${body_name}/${eyes_name}/*; do
+				faces_name=$(basename -s .svg ${faces})
+
+				output="generated/${outfit_name}/${body_name}/${eyes_name}/${faces_name}.png"
+
+				svgexport $faces $output 8x
+			done
+		done
+	done
+done
